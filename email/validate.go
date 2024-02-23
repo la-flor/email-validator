ae2183fd41d0833b010a19343d9fd5464265aab0
Leif Flor <leifaflor@gmail.com>
2024-02-22 22:02:11 -0700

    broken full email validity check

--- email/validate.go
+++ email/validate.go
@@ -9,6 +9,21 @@ import (
 	"time"
 )
 
+
+
+var mxToServerSMTPAddress = map[string]string{
+	"google": "smtp.gmail.com",
+	"smtpin.zoho.com": "smtp.zoho.com",
+	"mx.zoho.com": "smtp.zoho.com",
+	"mta6.am0.yahoodns.net": "smtp.mail.yahoo.com",
+	"o2": "smtp.o2.ie",
+	"mx-aol.mail.gm0.yahoodns.net": "smtp.aol.com",
+	"mx0b-00191d01.pphosted.com": "smtp.att.yahoo.com",
+	"hotmail-com.olc.protection.outlook.com": "smtp.live.com",
+	"mxb-00143702.gslb.pphosted.com": "smtp.comcast.net",
+	"mxa-0024a201.gslb.pphosted.com": "outgoing.verizon.net",
+}
+
 // Since not all servers allow address verification for
 // security reasons, we can only test if an address is
 // invalid and cannot guarantee that the address is valid.
@@ -33,6 +48,12 @@ func CheckIfInvalid(email string) (bool, string) {
 
 	if (invalidHost) {
 		return true, "Invalid host"
+	}
+
+	invalidEmail, _ := invalidFullEmail(Host, email)
+
+	if (invalidEmail) {
+		return true, "Email is invalid"
 	}
 
 	return false, "Email has not been proved invalid"
@@ -79,4 +100,36 @@ func dialTimeout(Host string) (*smtp.Client, error) {
 	defer conn.Close()
 
 	return smtp.NewClient(conn, Host)
+}
+
+
+
+func invalidFullEmail(Host string, email string) (bool, string) {
+	client, err := dialTimeout(Host)
+	if err != nil {
+		return true, "dialTimeout failed"
+	}
+
+	err = client.Hello(Host)
+	if err != nil {
+		return true, "Hello ping failed"
+	}
+
+	serverMailAddress := mxToServerSMTPAddress[Host]
+
+	if serverMailAddress == "" {
+		return false, "Unable to identify server address"
+	}
+
+	err = client.Mail(serverMailAddress)
+	if err != nil {
+		return true, "Server mail address failure"
+	}
+
+	err = client.Rcpt(email)
+	if err != nil {
+		return true, "RCPT failure"
+	}
+
+	return false, "No failure detected"
 }
